ea93005016ddc02b96bb6ecd346b09fd4fb83b3f
Neonoble <neo.munhenga@younglings.africa>
2022-09-16 11:58:57 +0200

    updated commit

--- src/components/SignupPage/Page.js
+++ src/components/SignupPage/Page.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import SignupInputs from './SignupInputs'
-import SignupBtn from './SignupBtn';
+
 
  function Page() {
   return (
@@ -9,12 +8,32 @@ import SignupBtn from './SignupBtn';
      <div className='card-body text-center d-flex flex-column align-items-center'>
        <h1 className='card-title fw-bold text-wrap'>Welcome Younglings!</h1>
 
-       <SignupInputs />
+      {/* sign up Inputs */}
+       <div className="w-75 mt-4 d-flex flex-column"  >
+		    	<div className='input-group mb-3'style={{witdh:'2000px'}}>
+		    		<input type="email" className='form-control p-2 fw-400 fs-5' placeholder='Email' required />
+		    	</div>
 
-       <SignupBtn />
+			    <div className='input-group mb-3'>
+			    	<input type='password' className='form-control p-2 fw-400 fs-5' placeholder='Name' />
+			    </div>
+
+			    <div className='input-group mb-2'>
+			    	<input type='password' className='form-control p-2 fw-400 fs-5' placeholder='Password' />
+			    </div>
+		   </div>
+
+        {/* sign up btn */}
+       <div className='d-grid mt-4 w-75'>
+        <button type='submit' className='btn btn-success p-2 fw-bold fs-4'>Sign up</button>
+       </div>
+      
 
        <div className='text-left mt-3 bottom-text fw-semibold smallText'style={{marginLeft:'15%'}}>
-         <span>Login</span> to an existing account
+         <span><a href="/" style={{
+           textDecoration:'none',
+           color:'#008800',
+         }}>Login</a></span> to an existing account
        </div>
      </div>
    </div>
